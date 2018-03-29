5681a75741d82e09b2f4bff8c48804e3288257d7
jinbin <nidnil@icloud.com>
2018-03-29 19:41:57 +0800

    openssl 1.0.2 -> 1.1.0

--- src/import/env.sh
+++ src/import/env.sh
@@ -32,7 +32,7 @@ MAKE_CA=https://raw.githubusercontent.com/djlucas/make-ca/master/make-ca; # text
 CERTDATA_DOWNLOAD=http://anduin.linuxfromscratch.org/BLFS/other/certdata.txt;
 CA_CERTIFICATES_REPOSITORY=https://salsa.debian.org/debian/ca-certificates.git;
 ZLIB_DOWNLOAD=http://www.zlib.net;
-OPENSSL_VERSION=1.0.2n;
+OPENSSL_VERSION=1.1.0h;
 OPENSSL_DOWNLOAD=https://www.openssl.org/source;
 OPENSSH_DOWNLOAD=http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable; # DROPBEAR_DOWNLOAD=https://matt.ucc.asn.au/dropbear;
 
