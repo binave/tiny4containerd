79d4d66d1cdd30dfdf7a8a0665339668cad4c129
jinbin <nidnil@icloud.com>
2018-03-29 20:39:27 +0800

    format code

--- src/rootfs/init.sh
+++ src/rootfs/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if mount -t tmpfs -o size=90% tmpfs /mnt; then
-  if tar -C / --exclude=mnt -cf - . | tar -C /mnt/ -xf - ; then
-    mkdir /mnt/mnt
-    exec /sbin/switch_root mnt /sbin/init
-  fi
+    if tar -C / --exclude=mnt -cf - . | tar -C /mnt/ -xf -; then
+        mkdir /mnt/mnt;
+        exec /sbin/switch_root mnt /sbin/init
+    fi
 fi
 
 # https://git.busybox.net/busybox/tree/examples/inittab
