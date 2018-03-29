8c6c013986912d1fdfd8fc2c183f771387d37036
jinbin <nidnil@icloud.com>
2018-03-29 20:39:57 +0800

    format code

--- src/import/profile.sh
+++ src/import/profile.sh
@@ -93,10 +93,10 @@ _apply_rootfs(){
     _mkcfg -$ROOTFS_DIR/init'
 #!/bin/sh
 if mount -t tmpfs -o size=90% tmpfs /mnt; then
-  if tar -C / --exclude=mnt -cf - . | tar -C /mnt/ -xf - ; then
-    mkdir /mnt/mnt
-    exec /sbin/switch_root mnt /sbin/init
-  fi
+    if tar -C / --exclude=mnt -cf - . | tar -C /mnt/ -xf -; then
+        mkdir /mnt/mnt;
+        exec /sbin/switch_root mnt /sbin/init
+    fi
 fi
 
 # https://git.busybox.net/busybox/tree/examples/inittab
