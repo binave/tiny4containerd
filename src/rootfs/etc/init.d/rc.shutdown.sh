d0f627c65e555b28179736ef852c64e1475fd31d
jinbin <nidnil@icloud.com>
2018-03-30 14:56:02 +0800

    format code

--- src/rootfs/etc/init.d/rc.shutdown.sh
+++ src/rootfs/etc/init.d/rc.shutdown.sh
@@ -5,81 +5,100 @@
 useBusybox
 
 clear
-Ymd=`date +%Y%m%d`
-LOG_DIR=/log/tiny/${Ymd:0:6}
-mkdir -p $LOG_DIR
+Ymd=`date +%Y%m%d`;
+LOG_DIR=/log/tiny/${Ymd:0:6};
+mkdir -p $LOG_DIR;
 
 {
-    printf "\n\n[`date`]\n"
+    printf "\n\n[`date`]\n";
 
     # stop container daemon
-    /usr/local/sbin/containerd stop
+    /usr/local/sbin/containerd stop;
 
     # shutdown script
     find /opt/tiny/etc/init.d -type f -perm /u+x -name "K*.sh" -exec /bin/sh -c {} \;
 
-    /usr/local/sbin/wtmp
+    /usr/local/sbin/wtmp;
 
     # PID USER COMMAND
     ps -ef | grep "crond\|monitor\|ntpd\|sshd\|udevd" | awk '{print "kill "$1}' | sh 2>/dev/null
 
-} 2>&1 | tee -a $LOG_DIR/shut_$Ymd.log
+} 2>&1 | tee -a $LOG_DIR/shut_$Ymd.log;
 
-unset LOG_DIR Ymd
+unset LOG_DIR Ymd;
 
 # Sync all filesystems.
-sync; sleep 1; sync; sleep 1
+sync; sleep 1; sync; sleep 1;
 
 # Unload disk
-/usr/local/sbin/mdisk destroy
+/usr/local/sbin/mdisk destroy;
 
-echo -n "${NORMAL}"
+echo -n "${NORMAL}";
 
 # Unmount all tcz extensions that were mounted into /tmp/tcloop via loopback
-for loop in $(mount | awk '/\/tmp\/tcloop/{print substr($1,10,3)}'|sort -nr); do umount -d /dev/loop"$loop" 2>/dev/null; done
+for loop in $(mount | awk '/\/tmp\/tcloop/{print substr($1,10,3)}'|sort -nr);
+do
+    umount -d /dev/loop"$loop" 2>/dev/null;
+done
+
 # Unmount all scm extensions that were mounted into /opt via loopback
-for loop in $(mount | awk '/\/opt/{print substr($1,10,3)}'|sort -nr); do umount -d /dev/loop"$loop" 2>/dev/null; done
+for loop in $(mount | awk '/\/opt/{print substr($1,10,3)}'|sort -nr);
+do
+    umount -d /dev/loop"$loop" 2>/dev/null;
+done
 
 if [ -s /tmp/audit_marked.lst ]; then
-   echo "${BLUE}Removing requested extensions:"
-   ONBOOTNAME="$(getbootparam lst 2>/dev/null)"
-   [ -n "$ONBOOTNAME" ] || ONBOOTNAME="onboot.lst"
-   for F in `cat /tmp/audit_marked.lst`; do
-      echo "${YELLOW}$F"
-      rm -f "$F"*
-      FROMDIR=`dirname "$F"` && TCEDIR=${FROMDIR%/*}
-      EXTN=`basename "$F"`; APP=${EXTN%.tcz}
-      LIST="$TCEDIR"/copy2fs.lst; ONBOOT="${TCEDIR}/${ONBOOTNAME}"; XWBAR="$TCEDIR"/xwbar.lst
-      if grep -w "$EXTN" "$LIST" >/dev/null 2>&1; then sed -i '/'"$EXTN"'/d' "$LIST"; fi
-      if grep -w "$EXTN" "$ONBOOT" >/dev/null 2>&1; then sed -i '/'"$EXTN"'/d' "$ONBOOT"; fi
-      if grep -w "$EXTN" "$XWBAR" >/dev/null 2>&1; then sed -i '/'"$EXTN"'/d' "$XWBAR"; fi
-      [ -s "$FROMDIR"/tce.db ] && rm -f "$FROMDIR"/tce.db
-      [ -s "$FROMDIR"/tce.lst ] && rm -f "$FROMDIR"/tce.lst
-      rm -f "$TCEDIR"/ondemand/$APP* 2>/dev/null
-   done
-   rm -f /tmp/audit_marked.lst
-   sync; sleep 1; sync; sleep 1
-   echo "${NORMAL}"
+    echo "${BLUE}Removing requested extensions:";
+    ONBOOTNAME="$(getbootparam lst 2>/dev/null)";
+    [ -n "$ONBOOTNAME" ] || ONBOOTNAME="onboot.lst";
+    for F in `cat /tmp/audit_marked.lst`;
+    do
+        echo "${YELLOW}$F";
+        rm -f "$F"*;
+        FROMDIR=`dirname "$F"` && TCEDIR=${FROMDIR%/*};
+        EXTN=`basename "$F"`;
+        APP=${EXTN%.tcz};
+        LIST="$TCEDIR"/copy2fs.lst;
+        ONBOOT="${TCEDIR}/${ONBOOTNAME}";
+        XWBAR="$TCEDIR"/xwbar.lst;
+        if grep -w "$EXTN" "$LIST" >/dev/null 2>&1; then
+            sed -i '/'"$EXTN"'/d' "$LIST";
+        fi
+        if grep -w "$EXTN" "$ONBOOT" >/dev/null 2>&1; then
+            sed -i '/'"$EXTN"'/d' "$ONBOOT";
+        fi
+        if grep -w "$EXTN" "$XWBAR" >/dev/null 2>&1; then
+            sed -i '/'"$EXTN"'/d' "$XWBAR";
+        fi
+        [ -s "$FROMDIR"/tce.db ] && rm -f "$FROMDIR"/tce.db;
+        [ -s "$FROMDIR"/tce.lst ] && rm -f "$FROMDIR"/tce.lst;
+        rm -f "$TCEDIR"/ondemand/$APP* 2>/dev/null
+    done
+
+    rm -f /tmp/audit_marked.lst;
+    sync; sleep 1; sync; sleep 1;
+    echo "${NORMAL}"
 fi
 
 # Umount filesystems.
-echo "${BLUE}Unmounting all filesystems. "
-echo -n "${NORMAL}"
+echo "${BLUE}Unmounting all filesystems. ";
+echo -n "${NORMAL}";
 
 TCE=$(readlink /etc/sysconfig/tcedir)
 if [ -d "$TCE" ]; then
-   TCEMOUNT=${TCE%/*}
-   [ -z "$TCEMOUNT" ] || umount "$TCEMOUNT" 2>/dev/null
+    TCEMOUNT=${TCE%/*};
+    [ -z "$TCEMOUNT" ] || umount "$TCEMOUNT" 2>/dev/null
 fi
 
 if [ -s /etc/sysconfig/backup_device ]; then
-   BACKUP=`cat /etc/sysconfig/backup_device`
-   BACKUPDEVICE=/mnt/${BACKUP%/*}
-   umount "$BACKUPDEVICE" 2>/dev/null
+    BACKUP=`cat /etc/sysconfig/backup_device`;
+    BACKUPDEVICE=/mnt/${BACKUP%/*};
+    umount "$BACKUPDEVICE" 2>/dev/null
 fi
 
-umount -arf >/dev/null 2>&1
+umount -arf >/dev/null 2>&1;
 
-echo "Shutdown in progress."
-sync
+echo "Shutdown in progress.";
+sync;
+
 echo
