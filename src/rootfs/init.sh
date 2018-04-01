23066361cdfafffe98a7fa84a0f5d0f58fb02383
jinbin <nidnil@icloud.com>
2018-04-02 00:08:40 +0800

    fix bug

--- src/rootfs/init.sh
+++ src/rootfs/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if mount -t tmpfs -o size=90% tmpfs /mnt; then
+    # fix: tar: skipping unsafe symlink to '...' in archive
+    export EXTRACT_UNSAFE_SYMLINKS=1;
     if tar -C / --exclude=mnt -cf - . | tar -C /mnt/ -xf -; then
         mkdir /mnt/mnt;
         exec /sbin/switch_root mnt /sbin/init
